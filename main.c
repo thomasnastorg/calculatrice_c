833d356091f2d44f4f860b20a46a840247127999
Thomas <nastorgt62@gmail.com>
2021-09-26 23:01:37 +0200

    allocation de mémoire dynamique ...

--- main.c
+++ main.c
@@ -7,14 +7,15 @@ int main(int argc, char *argv[]) {
 
     char *monOperation=NULL;
     monOperation = malloc(sizeof(char));
-
+    int ret;// taille de la chainne
         if (monOperation == NULL)
         {
             exit(0);
         }
         // demande a l'utilisateur de saisire
         write(1, "ecriver votre calcule:\n", 23);
-        scanf("%s", monOperation);
+       ret = read(0,monOperation,50);
+        printf("%s",monOperation);
 
 
 
