65cd2c99f1bce9155205a481377143df028eb69a
Thomas <nastorgt62@gmail.com>
2021-10-05 16:14:37 +0200

    yesy

--- calcullatriceFonction.c
+++ calcullatriceFonction.c
@@ -278,15 +278,14 @@ char* itoa(int num, char* str, int base)
     int i = 0;
     bool isNegative = false;
 
-    /* A zero is same "0" string in all base */
+
     if (num == 0) {
         str[i] = '0';
         str[i + 1] = '\0';
         return str;
     }
 
-    /* negative numbers are only handled if base is 10
-       otherwise considered unsigned number */
+
     if (num < 0 && base == 10) {
         isNegative = true;
         num = -num;
@@ -298,7 +297,7 @@ char* itoa(int num, char* str, int base)
         num = num/base;
     }
 
-    /* Append negative sign for negative numbers */
+
     if (isNegative){
         str[i++] = '-';
     }
