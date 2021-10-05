ed0e77bee88827e7fb2304525c0681a7bf5ce479
Thomas <nastorgt62@gmail.com>
2021-10-05 16:13:24 +0200

    itao

--- calcullatriceFonction.c
+++ calcullatriceFonction.c
@@ -10,6 +10,7 @@
 
 
 
+
 // fonction pour determinet la longeur de caracter
 int contsize(char *string){
     int i;
@@ -195,7 +196,7 @@ void moveAndWrit(char* decalageDeString,int positionA,int positionB, int nb){
     char buffer[20];
     int taille, position = positionA, tailleChar, decalageDeFin,fin;
 
-    transForme = itoa(nb,buffer,10);
+    transForme = itoa(nb, buffer, 10) ;
     taille = contsize(transForme);
     tailleChar = contsize(decalageDeString) ;
     decalageDeFin =positionB +1;
@@ -261,5 +262,50 @@ void checkIsAccepted(char *operation, int nbrchar){
         }
     }
 }
+void my_reverse(char str[], int len)
+{
+    int start, end;
+    char temp;
+    for(start=0, end=len-1; start < end; start++, end--) {
+        temp = *(str+start);
+        *(str+start) = *(str+end);
+        *(str+end) = temp;
+    }
+}
 
+char* itoa(int num, char* str, int base)
+{
+    int i = 0;
+    bool isNegative = false;
 
+    /* A zero is same "0" string in all base */
+    if (num == 0) {
+        str[i] = '0';
+        str[i + 1] = '\0';
+        return str;
+    }
+
+    /* negative numbers are only handled if base is 10
+       otherwise considered unsigned number */
+    if (num < 0 && base == 10) {
+        isNegative = true;
+        num = -num;
+    }
+
+    while (num != 0) {
+        int rem = num % base;
+        str[i++] = (rem > 9)? (rem-10) + 'A' : rem + '0';
+        num = num/base;
+    }
+
+    /* Append negative sign for negative numbers */
+    if (isNegative){
+        str[i++] = '-';
+    }
+
+    str[i] = '\0';
+
+    my_reverse(str, i);
+
+    return str;
+}
--- calcullatriceFonction.h
+++ calcullatriceFonction.h
@@ -15,6 +15,8 @@ void soutraction(char *operation,int position);
 void move(char* decalageDeString,int positionA,int positionB, int nb);
 void moveAndWrit(char* decalageDeString,int positionA,int positionB, int nb);
 void delParentheses(char *operatio, int positionParenthesesA, int positionParenthesesB,int tailleTotale);
+void my_reverse(char str[], int len);
+char* itoa(int num, char* str, int base);
 #endif //CALCULATRICE_C_CALCULLATRICEFONCTION_H
 
 
